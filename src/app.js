aad25f9fe1b5d543c29259696dca71ef842f039d
Chris Auret <chris.auret@yahoo.co.uk>
2017-01-31 22:58:31 +1100

    First

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header } from './components/common';
+import LoginForm from './components/loginForm';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDcwygOiPWAACX4L3X9pwjQ3AIt33KL2M4',
+      authDomain: 'react-native-auth-4b050.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-4b050.firebaseio.com',
+      storageBucket: 'react-native-auth-4b050.appspot.com',
+      messagingSenderId: '1068361324749'
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/loginForm.js
@@ -0,0 +1,40 @@
+
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+
+  state = { text: '' };
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 40, flex: 1 }}
+          />
+        </CardSection>
+        
+        <CardSection>
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 40, flex: 1 }}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Log in
+          </Button>
+        </CardSection>
+      </Card>
+
+    );
+  }
+}
+
+export default LoginForm;
