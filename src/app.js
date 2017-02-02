b72dc7b17712e0f37a456a07c2ade4773954d410
Chris Auret <chris.auret@yahoo.co.uk>
2017-02-02 22:03:50 +1100

    firebase auth onAuthStateChanged

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { Header } from './components/common';
 import LoginForm from './components/loginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDcwygOiPWAACX4L3X9pwjQ3AIt33KL2M4',
@@ -12,6 +14,12 @@ class App extends Component {
       databaseURL: 'https://react-native-auth-4b050.firebaseio.com',
       storageBucket: 'react-native-auth-4b050.appspot.com',
       messagingSenderId: '1068361324749'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      }
     });
   }
 
